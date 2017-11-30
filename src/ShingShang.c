2a9cefa468ae53b93f0abde38def35f70f730063
TancelinMazzotti <tancelin.mazzotti@gmail.com>
2017-11-30 10:31:25 +0100

    Alexis: Creation des pions des joueurs

--- src/ShingShang.c
+++ src/ShingShang.c
@@ -6,7 +6,9 @@ int run()
   Plateau plateau = construct_Plateau();
   Plateau_afficher(plateau);
   Joueur joueur1 = construct_Joueur(1,"Toto");
+  Joueur joueur2 = construct_Joueur(2,"Titi");
   Joueur_creerPion(&joueur1);
+  Joueur_creerPion(&joueur2);
 
   return 0;
 }
--- src/joueur.c
+++ src/joueur.c
@@ -12,24 +12,108 @@ Joueur construct_Joueur(int numero, char * nom)
 void Joueur_creerPion(Joueur * joueur)
 {
   int num_case_pion = 0;
-  int tmp;
-  int cpt=4;
-  
-  for (int y = 0; y < 3; y++)
+  int abscisseMIN;
+	int abscisseMAX;
+	int ordonneeMIN;
+	int ordonneeMax;
+	int valeurPionMAX;
+	int valeurPion;
+
+  if (joueur->numero == 1)
   {
-    for (int x = 1; x < cpt; x++)
+    printf("Joueur %d:\n",joueur->numero);
+    abscisseMIN = 1;
+  	abscisseMAX = 4;
+  	ordonneeMIN = 0;
+  	ordonneeMax = 3;
+  	valeurPionMAX = 3;
+  	valeurPion;
+
+  	for(int y = ordonneeMIN; y < ordonneeMax; y++)
+  	{
+  		valeurPion = valeurPionMAX;
+  		for(int x = abscisseMIN; x < abscisseMAX; x++)
+  		{
+        joueur->tab_p_pion[num_case_pion] = construct_Pion(joueur->numero, valeurPion, x, y);
+  			//printf("%d[%d;%d] ",valeurPion,x,y);
+        printf("%d[%d;%d] ",joueur->tab_p_pion[num_case_pion].typePion,joueur->tab_p_pion[num_case_pion].x,joueur->tab_p_pion[num_case_pion].y);
+  			valeurPion--;
+        num_case_pion++;
+  		}
+  		printf("\n");
+  		abscisseMAX--;
+  		valeurPionMAX--;
+  	}
+
+    abscisseMIN = 6;
+  	abscisseMAX = 9;
+  	ordonneeMIN = 0;
+    ordonneeMax = 3;
+    valeurPionMAX = 1;
+
+    for(int y = ordonneeMIN; y < ordonneeMax; y++)
     {
-      tmp = 4;
-      tmp = tmp - x - y;
-      joueur->tab_p_pion[num_case_pion] = construct_Pion(joueur->numero, tmp, x, y);
-
-      printf("%d",joueur->tab_p_pion[num_case_pion].typePion );
-      printf("x=%d y=%d",joueur->tab_p_pion[num_case_pion].x,joueur->tab_p_pion[num_case_pion].y);
+      valeurPion = 1;
+      for(int x = abscisseMIN; x < abscisseMAX; x++)
+      {
+        joueur->tab_p_pion[num_case_pion] = construct_Pion(joueur->numero, valeurPion, x, y);
+        //printf("%d[%d;%d] ",valeurPion,x,y);
+        printf("%d[%d;%d] ",joueur->tab_p_pion[num_case_pion].typePion,joueur->tab_p_pion[num_case_pion].x,joueur->tab_p_pion[num_case_pion].y);
+        valeurPion++;
+        num_case_pion++;
+      }
       printf("\n");
-
-      num_case_pion++;
+      abscisseMIN++;
     }
-    cpt--;
+    printf("\n");
+  }
+
+  else if (joueur->numero == 2)
+  {
+    printf("Joueur %d:\n",joueur->numero);
+    abscisseMIN = 1;
+    abscisseMAX = 2;
+    ordonneeMIN = 7;
+    ordonneeMax = 10;
+    valeurPionMAX = 1;
+
+    for(int y = ordonneeMIN; y < ordonneeMax; y++)
+    {
+      valeurPion = valeurPionMAX;
+      for(int x = abscisseMIN; x < abscisseMAX; x++)
+      {
+        joueur->tab_p_pion[num_case_pion] = construct_Pion(joueur->numero, valeurPion, x, y);
+        //printf("%d[%d;%d] ",valeurPion,x,y);
+        printf("%d[%d;%d] ",joueur->tab_p_pion[num_case_pion].typePion,joueur->tab_p_pion[num_case_pion].x,joueur->tab_p_pion[num_case_pion].y);
+        valeurPion--;
+        num_case_pion++;
+      }
+      printf("\n");
+      valeurPionMAX++;
+      abscisseMAX++;
+    }
+
+	  abscisseMIN = 8;
+    abscisseMAX = 9;
+    ordonneeMIN = 7;
+    ordonneeMax = 10;
+    valeurPionMAX = 1;
+
+    for(int y = ordonneeMIN; y < ordonneeMax; y++)
+    {
+      valeurPion = 1;
+      for(int x = abscisseMIN; x < abscisseMAX; x++)
+      {
+        joueur->tab_p_pion[num_case_pion] = construct_Pion(joueur->numero, valeurPion, x, y);
+        //printf("%d[%d;%d] ",valeurPion,x,y);
+        printf("%d[%d;%d] ",joueur->tab_p_pion[num_case_pion].typePion,joueur->tab_p_pion[num_case_pion].x,joueur->tab_p_pion[num_case_pion].y);
+        valeurPion++;
+        num_case_pion++;
+      }
+      printf("\n");
+      valeurPionMAX++;
+      abscisseMIN--;
+    }
     printf("\n");
   }
 }
